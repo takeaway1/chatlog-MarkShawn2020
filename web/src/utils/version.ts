61846da08f779d036e6070a7ce6f42b99a1a292b
markshawn2020 <shawninjuly@gmail.com>
2025-11-09 14:00:40 +0800

    chore: update version.ts to latest commit

--- web/src/utils/version.ts
+++ web/src/utils/version.ts
@@ -5,7 +5,7 @@
 import packageJson from '../../package.json';
 
 // Auto-updated by pre-commit hook - DO NOT EDIT MANUALLY
-export const GIT_COMMIT_HASH = '373d674';
+export const GIT_COMMIT_HASH = 'cd29e3e';
 export const GIT_COMMIT_DATE = '2025-11-09';
 
 export const APP_VERSION = `${packageJson.version}-${GIT_COMMIT_HASH}`;
